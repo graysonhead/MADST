be3579e8c78c907d7b30a17915a6ec2a970c5d80
Grayson Head <grayson@graysonhead.net>
2018-01-07 01:00:48 -0600

    Basic API organization filtering

--- app/views_api.py
+++ app/views_api.py
@@ -11,11 +11,14 @@ from app import db, models
 # 		}
 # }
 
-def get_tasks():	
+def get_tasks(org_id=False):
 	sesh = db.session()
 	try:
 		tasks = {}
-		db_tasks = sesh.query(models.Task).all()
+		if org_id:
+			db_tasks = sesh.query(models.Task).filter_by(organization_id=org_id).all()
+		else:
+			db_tasks = sesh.query(models.Task).all()
 		for i, val in enumerate(db_tasks):
 			task_item = {
 				'id': val.id,
@@ -65,6 +68,11 @@ class Tasks(Resource):
 		tasks = get_tasks()
 		return tasks
 
+class Tasks_filtered(Resource):
+	def get(self, org_id):
+		tasks = get_tasks(org_id)
+		return tasks
+
 class Tasks_Count(Resource):
 	def get(self):
 		tasks = get_tasks()
@@ -74,5 +82,6 @@ class Tasks_Count(Resource):
 
 api.add_resource(Tasks_Count, '/api/tasks/count')
 api.add_resource(Tasks, '/api/tasks')
+api.add_resource(Tasks_filtered, '/api/tasks/<org_id>')
 api.add_resource(Task, '/api/task/<task_id>')
 api.add_resource(Tasks, )
