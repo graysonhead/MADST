179218e291d40d3f5a90421460bc1f809646f324
Grayson Head <grayson@graysonhead.net>
2018-01-11 16:06:23 -0600

    MADST-client improvements

--- MADST-client.py
+++ MADST-client.py
@@ -3,12 +3,14 @@ from Crypto.Cipher import AES
 import base64
 from pyad import adbase, adcomputer, adcontainer, adsearch, adquery, addomain, pyad, aduser, adgroup, pyadexceptions
 import re
+import time
+
 
 host = 'http://10.233.51.213:5000/'
 org_id = 3
 private_key = 'MAW9G6IHEQIANE2UD1YE4SCFIZGY6D3Y'
-
-r = requests.get('http://10.233.51.213:5000/api/tasks/{}'.format(org_id))
+updated_users = 0
+added_users = 0
 
 def get_ou(ou):
 	try:
@@ -49,40 +51,38 @@ def change_task_status(task_id, status_id):
 	requests.put(host + 'api/task/' + str(task_id), data={'status': status_id})
 
 
-''' Lets do stuff '''
-password = r.json()['0']['user']['sync_password']
-password = decrypt(password, private_key).decode('utf-8')
-updated_users = 0
-added_users = 0
 
-for key, value in r.json().items():
-	if value['status']['id'] == 1:
-		cn = value['user']['first_name'] + ' ' + value['user']['last_name']
-		password = value['user']['sync_password']
-		password = decrypt(password, private_key).decode('utf-8')
+while True:
+	r = requests.get('http://10.233.51.213:5000/api/tasks/{}'.format(org_id))
+	for key, value in r.json().items():
+		if value['status']['id'] == 1:
+			cn = value['user']['first_name'] + ' ' + value['user']['last_name']
+			password = value['user']['sync_password']
+			password = decrypt(password, private_key).decode('utf-8')
 
-		if check_user_exists(cn):
-			try:
-				update_password(cn, password)
-				print('Updated User {}'.format(cn))
-				updated_users = updated_users + 1
-				change_task_status(value['id'], '3')
-			except:
-				change_task_status(value['id'], '4')
+			if check_user_exists(cn):
+				try:
+					update_password(cn, password)
+					print('Updated User {}'.format(cn))
+					updated_users = updated_users + 1
+					change_task_status(value['id'], '3')
+				except:
+					change_task_status(value['id'], '4')
 
+			else:
+				# User doesn't exist, create them
+				try:
+					add_ad_user(get_ou(value['organization']['admin_ou']), cn)
+					print('Added User {}'.format(cn))
+					added_users = added_users + 1
+					change_task_status(value['id'], '3')
+				except:
+					change_task_status(value['id'], '4')
 		else:
-			# User doesn't exist, create them
-			try:
-				add_ad_user(get_ou(value['organization']['admin_ou']), cn)
-				print('Added User {}'.format(cn))
-				added_users = added_users + 1
-				change_task_status(value['id'], '3')
-			except:
-				change_task_status(value['id'], '4')
-	else:
-		print('No new issues')
+			print('No new issues')
 
-if added_users:
-	print("Added {} users".format(added_users))
-if updated_users:
-	print("Updated {} users".format(updated_users))
+	# if added_users:
+	# 	print("Added {} users".format(added_users))
+	# if updated_users:
+	# 	print("Updated {} users".format(updated_users))
+	time.sleep(5)
