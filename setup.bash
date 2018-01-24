1d7b863fdba2879223ed3bcb388bb2a6e1f0c586
root <root@adst-app.localdomain>
2018-01-24 09:02:26 -0600

    Setup script changes

--- setup.bash
+++ setup.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+yum install python34 python34-devel python34-crypto python34-pip gcc make -y
 python3 -m venv ./venv
-./venv/bin/python3-pip install Flask Flask-Login Flask-RESTful Flask-SQLAlchemy Flask-WTF SQLAlchemy WTForms pycrypto requests 
+./venv/bin/pip3 install Flask Flask-Login Flask-RESTful Flask-SQLAlchemy Flask-WTF SQLAlchemy WTForms pycrypto requests 
 
