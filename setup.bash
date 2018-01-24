55ee5b37f44c5d3ec26320c8a9a5f6be40dec601
Grayson Head <grayson.head@higher-state.com>
2018-01-24 09:22:50 -0600

    Modifying setup script

--- setup.bash
+++ setup.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum install python34 python34-devel python34-crypto python34-pip gcc make -y
 python3 -m venv ./venv
-./venv/bin/pip3 install Flask Flask-Login Flask-RESTful Flask-SQLAlchemy Flask-WTF SQLAlchemy WTForms pycrypto requests 
+./venv/bin/pip3 install Flask Flask-Login Flask-RESTful Flask-SQLAlchemy Flask-WTF SQLAlchemy sqlalchemy-migrate WTForms pycrypto requests 
 
