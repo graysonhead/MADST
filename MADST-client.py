2ca9bb68a497f1ce3d05777b476d9fd6da82ab5e
Spencer Harmon <spencer@RSITEX.COM>
2018-01-25 14:56:53 -0600

    MADST-3: RESOLVED use pycryptodomex instead of pycrypto. Removes visual studio dependency and works as expected. I'm not sure if it adds a dependency on mingw that isn't provided by the exe. App still broken due to hardcoded IP, et c.

--- MADST-client.py
+++ MADST-client.py
@@ -1,5 +1,5 @@
 import requests
-from Crypto.Cipher import AES
+from Cryptodome.Cipher import AES
 import base64
 from pyad import adbase, adcomputer, adcontainer, adsearch, adquery, addomain, pyad, aduser, adgroup, pyadexceptions
 import re
--- build.bat
+++ build.bat
@@ -1,4 +1,1 @@
-
-python C:\Users\graysonh\Documents\pyinstaller-develop\pyinstaller.py --onefile --specpath c:\Users\graysonh\Documents\HSTAD\build --clean c:\Users\graysonh\Documents\HSTAD\hstad.py
-
-python pyinstaller --onefile --specpath C:\Users\grayson\PycharmProjects\HSTAD\build --clean C:\Users\grayson\PycharmProjects\HSTAD\MADST-client.py
+pyinstaller --onefile --specpath build --clean MADST-client.py
