dba62d90e72f13fbbd677cdcd6d4beffae07d882
Grayson Head <grayson@graysonhead.net>
2018-01-26 22:57:26 -0600

    Added ability to delete organizations from the org admin screen. Added ability to delete user-templates from template admin screen. Added ability to resync all users within organization.

--- app/views.py
+++ app/views.py
@@ -108,39 +108,80 @@ def admin():
 @app.route('/admin/org', methods=['GET', 'POST'])
 def admin_org():
 	""" Allows viewing and modification of Organization Attributes"""
-	org_id = request.args.get('org_id', default = 1, type = int)
+	org_id = request.args.get('org_id', default=1, type=int)
+	delete = request.args.get('delete', default=0, type=int)
+	resync = request.args.get('resync', default=0, type=int)
 	form = AddName()
-	if form.name.data:
+	if request.method == 'POST':
+		if form.name.data:
+			sesh = db.session()
+			try:
+				org = sesh.query(models.Organization).filter_by(id=org_id).first()
+				org.add_template(form.name.data)
+				sesh.add(org)
+				sesh.commit()
+				flash("Added {} to {}".format(form.name.data, org.name))
+			except:
+				sesh.rollback()
+				flash("DB write failed", 'error')
+			finally:
+				sesh.close()
+		return(redirect(url_for("admin_org", org_id = org_id)))
+	if request.method == 'GET':
+		if delete == 1:
+			sesh = db.session()
+			try:
+				org = sesh.query(models.Organization).filter_by(id=org_id).first()
+				for tmp in org.templates:
+					# Clean up single attributes in org
+					for sa in tmp.single_attributes:
+						sesh.delete(sa)
+					# Clean up multi attributes in org
+					for ma in tmp.multi_attributes:
+						sesh.delete(ma)
+					# Clean up templates in org
+					sesh.delete(tmp)
+				# Clean up tasks in org
+				for t in org.tasks:
+					sesh.delete(t)
+				# Delete org
+				sesh.delete(org)
+				sesh.commit()
+				flash("Deleted organization {}.".format(org.name))
+			except:
+				sesh.rollback()
+				flash("Database error occurred while deleting organization {}".format(org.name))
+			finally:
+				sesh.close()
+			return(redirect(url_for('admin_orgs')))
+		elif resync == 1:
+			sesh = db.session()
+			try:
+				org = sesh.query(models.Organization).filter_by(id=org_id).first()
+				for u in org.admin_users:
+					org.add_task(u)
+				flash("Created sync tasks for all admin users in organization {}".format(org.name))
+				sesh.commit()
+			except:
+				sesh.rollback()
+			finally:
+				sesh.close()
+			return(redirect(url_for('admin_org', org_id=org_id)))
 		sesh = db.session()
 		try:
 			org = sesh.query(models.Organization).filter_by(id=org_id).first()
-			org.add_template(form.name.data)
-			sesh.add(org)
-			sesh.commit()
-			flash("Added {} to {}".format(form.name.data, org.name))
+			templates = org.templates
 		except:
 			sesh.rollback()
-			flash("DB write failed", 'error')
 		finally:
 			sesh.close()
-		return(redirect(url_for("admin_org")))
-	sesh = db.session()
-	try:
-		org = sesh.query(models.Organization).filter_by(id=org_id).first()
-		templates = org.templates
-	except:
-		sesh.rollback()
-	finally:
-		sesh.close()
-
-
-	return render_template(
-		'admin_org.html',
-		form=form,
-		title='Organization Admin: {}'.format(org.name.title()),
-		org=org,
-		templates=templates
-	)
+		return render_template(
+			'admin_org.html',
+			form=form,
+			title='Organization Admin: {}'.format(org.name.title()),
+			org=org,
+			templates=templates
+		)
 
 @required('Admin')
 @login_required
@@ -149,12 +190,15 @@ def admin_org_template(**kwargs):
 	""" Allows viewing and modification of Organization Attributes"""
 	selected_single_attribute = request.args.get('selected_single_attribute', default=0, type=int)
 	selected_multi_attribute = request.args.get('selected_multi_attribute', default=0, type=int)
+	template_id = request.args.get('template_id', default=1, type=int)
+	delete = request.args.get('delete', default=0, type=int)
 	""" Forms """
 	svform = KeyValueModify()
 	new_sv_form = KeyValueAdd()
 	form = AddName()
 	mvform = MultiKeyValueModify()
 	new_mvform = MultiKeyValueAdd()
+
 	if request.method == 'POST':
 		template_id = request.args.get('template_id', default=1, type=int)
 		""" Single Value Logic """
@@ -227,9 +271,25 @@ def admin_org_template(**kwargs):
 				sesh.close()
 		return (redirect(url_for('admin_org_template', template_id=template_id)))
 	if request.method == 'GET':
+		if delete == 1:
+			sesh = db.session()
+			try:
+				template = sesh.query(models.UserTemplate).filter_by(id=template_id).first()
+				orgid = template.organization
+				for sa in template.single_attributes:
+					sesh.delete(sa)
+				for ma in template.multi_attributes:
+					sesh.delete(ma)
+				sesh.delete(template)
+				sesh.commit()
+			except:
+				sesh.rollback()
+			finally:
+				sesh.close()
+			return(redirect(url_for('admin_org', admin_org=orgid)))
 		sesh = db.session()
 		try:
-			template = sesh.query(models.UserTemplate).filter_by(id=request.args.get('template_id', default=1, type=int)).first()
+			template = sesh.query(models.UserTemplate).filter_by(id=template_id).first()
 			org = sesh.query(models.Organization).filter_by(id=template.organization).first()
 			templates = org.templates
 			single_attributes = template.single_attributes
