959b2618833e08a08efbec6cb817f13272fb714d
Grayson Head <grayson@graysonhead.net>
2018-02-04 15:21:15 -0600

    MADST-7 API Refactor (Removed flask-RESTful in favor of some custom decorators)

--- app/decorators.py
+++ app/decorators.py
@@ -1,5 +1,8 @@
 from functools import wraps
 from werkzeug.exceptions import Forbidden
+from app import db, models
+from flask import jsonify, request
+
 
 def import_user():
 	try:
@@ -10,6 +13,21 @@ def import_user():
 			'User argument not passed and current_user could not be imported.'
 		)
 
+
+def api_auth(apikey, secret):
+	sesh = db.session()
+	try:
+		org = sesh.query(models.Organization).filter_by(apikey=apikey).first()
+		if org.apikey == apikey and org.sync_key == secret:
+			return True
+		else:
+			return False
+	except:
+		sesh.rollback()
+	finally:
+		sesh.close()
+
+
 def required(role):
 	def wrapper(func):
 		@wraps(func)
@@ -19,6 +37,19 @@ def required(role):
 			if current_user.check_role(role):
 				return func(*args, **kwargs)
 			raise Forbidden("Your roles do not grant you access to this page")
+		return inner
+	return wrapper
+
+
+def api_key_required():
+	def wrapper(func):
+		@wraps(func)
+		def inner(*args, **kwargs):
+			apikey = request.args.get('apikey', default='', type=str)
+			secret = request.args.get('secret', default='', type=str)
+			if api_auth(apikey, secret) is True:
+				return func(*args, **kwargs)
+			return jsonify({"Error": "Incorrect authentication or not authorized."}), 401
 		return inner
 	return wrapper
 
--- app/views_api.py
+++ app/views_api.py
@@ -1,27 +1,8 @@
-from flask_restful import reqparse, abort, Api, Resource
-from app import api
+from flask import request
 from flask import jsonify
-from app import db, models, crypt
+from app import db, models, crypt, app
+from .decorators import api_key_required
 import re
-from functools import wraps
-
-key_parser = reqparse.RequestParser()
-key_parser.add_argument('apikey')
-key_parser.add_argument('secret')
-
-
-def api_auth(apikey, secret):
-	sesh = db.session()
-	try:
-		org = sesh.query(models.Organization).filter_by(apikey=apikey).first()
-		if org.apikey == apikey and org.sync_key == secret:
-			return False
-		else:
-			return True
-	except:
-		sesh.rollback()
-	finally:
-		sesh.close()
 
 
 def atrib_regex(username, firstname, lastname, string):
@@ -31,7 +12,65 @@ def atrib_regex(username, firstname, lastname, string):
 	return re.sub(r"%lastName%", lastname, string)
 
 
-def get_task(task_id):
+def tasks_fetch(org_id=False):
+	sesh = db.session()
+	try:
+		tasks = {}
+		if org_id:
+			db_tasks = sesh.query(models.Task).filter_by(organization_id=org_id).all()
+		else:
+			db_tasks = sesh.query(models.Task).all()
+		for i, val in enumerate(db_tasks):
+			task_item = {
+				'id': val.id,
+				'type': 'create',
+				'status': {
+					'id': val.status.id,
+					'name': val.status.name
+				},
+				'organization': {
+					'id': val.organization_id,
+					'name': val.organization.name,
+					'admin_ou': val.organization.admin_ou
+				},
+				'user': {
+					'first_name': val.user.first_name.title(),
+					'last_name': val.user.last_name.title(),
+					'sync_username': val.user.sync_username,
+					'sync_password': crypt.encrypt(val.user.sync_password, val.organization.sync_key).decode('utf-8')
+				},
+				'attributes': {
+					'single_attributes': {},
+					'multi_attributes': {}
+				}
+			}
+			try:
+				if val.organization.templates[0].single_attributes[0]:
+					for s in val.organization.templates[0].single_attributes:
+						task_item['attributes']['single_attributes'].update({s.key: atrib_regex(val.user.sync_username,
+																								val.user.first_name,
+																								val.user.last_name,
+																								s.value)})
+			except IndexError:
+				pass
+			try:
+				if val.organization.templates[0].multi_attributes[0]:
+					for s in val.organization.templates[0].multi_attributes:
+						task_item['attributes']['multi_attributes'].update({s.key: atrib_regex(val.user.sync_username,
+																							   val.user.first_name,
+																							   val.user.last_name,
+																							   s.value)})
+			except IndexError:
+				pass
+			tasks.update({str(i): task_item})
+		return tasks
+	except:
+		sesh.rollback()
+	finally:
+		sesh.close()
+
+
+def task_fetch(task_id):
 	sesh = db.session
 	try:
 		val = sesh.query(models.Task).filter_by(id=task_id).first()
@@ -77,137 +116,42 @@ def get_task(task_id):
 	return task_item
 
 
-def get_tasks(org_id=False):
-	sesh = db.session()
-	args = key_parser.parse_args()
-	try:
-		# Check for auth
-		tasks = {}
-		if org_id:
-			db_tasks = sesh.query(models.Task).filter_by(organization_id=org_id).all()
-		else:
-			db_tasks = sesh.query(models.Task).all()
-		for i, val in enumerate(db_tasks):
-			task_item = {
-				'id': val.id,
-				'type': 'create',
-				'status': {
-					'id': val.status.id,
-					'name': val.status.name
-				},
-				'organization': {
-					'id': val.organization_id,
-					'name': val.organization.name,
-					'admin_ou': val.organization.admin_ou
-				},
-				'user': {
-					'first_name': val.user.first_name.title(),
-					'last_name': val.user.last_name.title(),
-					'sync_username': val.user.sync_username,
-					'sync_password': crypt.encrypt(val.user.sync_password, val.organization.sync_key).decode('utf-8')
-				},
-				'attributes': {
-					'single_attributes': {},
-					'multi_attributes': {}
-				}
-			}
-			try:
-				if val.organization.templates[0].single_attributes[0]:
-					for s in val.organization.templates[0].single_attributes:
-							task_item['attributes']['single_attributes'].update({s.key: atrib_regex(val.user.sync_username, val.user.first_name, val.user.last_name, s.value)})
-			except IndexError:
-				pass
-			try:
-				if val.organization.templates[0].multi_attributes[0]:
-					for s in val.organization.templates[0].multi_attributes:
-							task_item['attributes']['multi_attributes'].update({s.key: atrib_regex(val.user.sync_username, val.user.first_name, val.user.last_name, s.value)})
-			except IndexError:
-				pass
-			tasks.update({str(i): task_item})
-			return tasks
-		else:
-			return {"Error": "Incorrect authentication or not authorized."}
-	except:
-		sesh.rollback()
-		return {"Error": "True"}
-	finally:
-		sesh.close()
-
-
-
-def abort_no_task(task_id):
-	if task_id not in tasks:
-		abort(404, message="Task {} doesn't exist".format(task_id))
-
-def abort_no_status(status_id):
-	abort(404, message="Status {} doesn't exist".format(status_id))
-
-
-parser = reqparse.RequestParser()
-parser.add_argument('status')
-
-
-class Task(Resource):
-	def get(self, task_id):
-		keyargs = key_parser.parse_args()
-		if api_auth(keyargs['apikey'], keyargs['secret']):
-			return {"Error": True, "Message": "Auth Failure"}
-		task = get_task(task_id)
-		return task
-
-	def put(self, task_id):
-		keyargs = key_parser.parse_args()
-		if api_auth(keyargs['apikey'], keyargs['secret']):
-			return {"Error": True, "Message": "Auth Failure"}
-		args = parser.parse_args()
+@app.route('/api/task/<task_id>', methods=['GET', 'PUT', 'POST'])
+@api_key_required()
+def api_task(task_id):
+	# Begin GET block
+	if request.method == 'GET':
+		return jsonify(task_fetch(task_id))
+	# End GET block
+	# Begin PUT block
+	elif request.method == 'PUT':
+		try:
+			data = request.get_json()
+			status_id = data['status']
+		except AttributeError:
+			return jsonify({"Error": "Bad request data", "RequestData": data}), 406
+		except KeyError:
+			return jsonify({"Error": "A non-optional key is missing, please check the documentation and try again", "RequestData": data}), 406
 		sesh = db.session()
 		try:
 			taskdb = sesh.query(models.Task).filter_by(id=task_id).first()
 			try:
-				taskdb.change_status(args['status'])
+				taskdb.change_status(status_id)
 			except:
-				return abort_no_status(args['status'])
+				return jsonify({"Error": "No status code {} exists".format(status_id)}), 404
 			sesh.add(taskdb)
 			sesh.commit()
 		except:
 			sesh.rollback()
-			return abort_no_task(task_id)
+			return jsonify({"Error": "There is no task with id{}".format(task_id)}), 404
 		finally:
 			sesh.close()
-		return get_task(task_id), 201
-	# def put(self, task_id):
-	# 	args = parser.parse_args()
+		return jsonify(task_fetch(task_id)), 201
 
 
-class Tasks(Resource):
-	def get(self):
-		keyargs = key_parser.parse_args()
-		if api_auth(keyargs['apikey'], keyargs['secret']):
-			return {"Error": True, "Message": "Auth Failure"}
-		tasks = get_tasks()
-		return tasks
+def get_tasks(org_id=False):
+	if org_id:
+		return jsonify(tasks_fetch(org_id))
+	else:
+		return jsonify(tasks_fetch())
 
-
-class Tasks_filtered(Resource):
-	def get(self, org_id):
-		keyargs = key_parser.parse_args()
-		if api_auth(keyargs['apikey'], keyargs['secret']):
-			return {"Error": True, "Message": "Auth Failure"}
-		tasks = get_tasks(org_id)
-		return tasks
-
-class Tasks_Count(Resource):
-	def get(self):
-		keyargs = key_parser.parse_args()
-		if api_auth(keyargs['apikey'], keyargs['secret']):
-			return {"Error": True, "Message": "Auth Failure"}
-		tasks = get_tasks()
-		return { 'count': len(tasks)}
-
-
-
-api.add_resource(Tasks_Count, '/api/tasks/count')
-api.add_resource(Tasks, '/api/tasks')
-api.add_resource(Tasks_filtered, '/api/tasks/<org_id>')
-api.add_resource(Task, '/api/task/<task_id>')
-api.add_resource(Tasks, )
