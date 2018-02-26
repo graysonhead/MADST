acf976f7f562b5762c9ab5c0e16ea07f4e6fc41f
Spencer Harmon <spencer@RSITEX.COM>
2018-02-25 18:44:26 -0600

    user/pw in config for now

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ host = 'http://66.194.167.189:5000/'
 org_id = 1
 apikey = '3020013fff2675f3b3e1bcdd2bb65f20af2a'
 private_key = '908HBI8TAUD2FLFOZ5WFFQIM77YIADN9'
+username = 'administrator'
+password = ''
+domain = ''
