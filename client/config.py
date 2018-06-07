15abe8366df402d3f94d5218f17957ad9e33fe7a
Spencer Harmon <spencerharmon.91@gmail.com>
2018-06-07 14:20:15 -0500

    allow tasks to update existing users' attributes in addition to password, e.g. for changes in the template

--- client/config.py
+++ client/config.py
@@ -3,8 +3,8 @@
 
 host = 'http://192.168.128.147:5000/'
 org_id = 1
-apikey = 'd43c98159b2d256f3c80401303a3cfd8ac97'
-private_key = 'JYF9N0TCQP2ZHAB3W52JWJGCD38YREDQ'
+apikey = '4b2fda2683ee2043732ea02bb70ebba3cd85'
+private_key = 'PHQLBCI5M5UPRUDA8AKB5FMFA7ADV3VR'
 ldap_server = '127.0.0.1'
 username = 'administrator'
 password = '1qazxsW@'
