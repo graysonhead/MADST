81a1109c6ea351867c3b742cafd6a418cf9c8e95
Spencer Harmon <spencerharmon.91@gmail.com>
2018-06-08 09:25:17 -0500

    remove testing config

---
+++ client/config.example.py
@@ -0,0 +1,13 @@
+#todo:  host and keys in local (not python) config, the rest
+#served from api
+
+host = '<MADST Server URI>'
+org_id = 'int ID of organization from MADST Server'
+apikey = '<get key from madst server organization>'
+private_key = '<get key from madst server organization>'
+ldap_server = '127.0.0.1'
+username = '<domain user with read access to billing group DN>'
+password = '<password for above user>'
+domain = '<netbios name of domain>'
+baseDN = '<Base DN of client domain>'
+ldap_server_type=1 #1 for windows AD
