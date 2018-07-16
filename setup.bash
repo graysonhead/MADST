7040c21f82d2b055ec5dfca5e4ad1500dad56cf1
Spencer Harmon <spencer.harmon@higher-state.com>
2018-07-16 16:35:38 +0000

    added uwsgi configuration details

--- setup.bash
+++ setup.bash
@@ -5,4 +5,4 @@ elif [ -f "/etc/debian_version" ]; then
 	apt install python3 python3-dev python3-pip python3-venv gcc make -y
 python3 -m venv ./venv
 fi
-./venv/bin/pip3 install Flask Flask-Login Flask-RESTful Flask-SQLAlchemy Flask-WTF SQLAlchemy sqlalchemy-migrate WTForms pycrypto requests flask_apscheduler ldap3
+pip install Flask Flask-Login Flask-RESTful Flask-SQLAlchemy Flask-WTF SQLAlchemy sqlalchemy-migrate WTForms pycrypto requests flask_apscheduler ldap3 uwsgi
