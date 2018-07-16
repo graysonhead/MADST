ebf3def11966d50b9034da73136ccdce9d04cd51
Spencer Harmon <spencer.harmon@higher-state.com>
2018-07-16 14:40:58 +0000

    replaced generic run.py with debug and wsgi

---
+++ debug.py
@@ -0,0 +1,5 @@
+#!venv/bin/python
+from app import app
+
+if __name__ == "__main__":
+	app.run(debug=True, use_reloader=False, host='0.0.0.0')
--- run.py
+++
@@ -1,4 +0,0 @@
-#!venv/bin/python
-from app import app
-
-app.run(debug=True, use_reloader=False, host='0.0.0.0')
